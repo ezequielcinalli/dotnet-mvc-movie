e39d780f3b29c1712753400f27ef55e8828ad63b
ezequiel <ezequielcinalli96@gmail.com>
2021-11-28 19:13:56 -0300

    Part 8, add a new field

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -65,7 +65,7 @@ namespace MvcMovie.Controllers {
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie) {
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
             if (ModelState.IsValid) {
                 _context.Add(movie);
                 await _context.SaveChangesAsync();
@@ -92,7 +92,7 @@ namespace MvcMovie.Controllers {
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie) {
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
             if (id != movie.Id) {
                 return NotFound();
             }
